900bff245c19b2068d30f67b6fb5637ba7072821
haoxin <haoxinst@gmail.com>
2019-10-15 17:46:25 +0800

    rm thunkify

---
+++ test/promise.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var should = require('should'),
+  config = require('./config'),
+  OSS = require('..');
+
+describe('# promisify', function() {
+  var oss = OSS.createClient(config);
+
+  it('should list bucket', async function() {
+    const res = await oss.listBucket();
+
+    res.status.should.equal(200);
+    res.body.ListAllMyBucketsResult.should.have.keys('Owner', 'Buckets');
+  });
+});
